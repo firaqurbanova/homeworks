d99cb787ef1e7a3fc2272ce3e82c75701bb45127
firaqurbanova <58509855+firaqurbanova@users.noreply.github.com>
2020-02-19 16:39:27 +0400

    Update hw2.js

--- homework(js)/hw2/js/hw2.js
+++ homework(js)/hw2/js/hw2.js
@@ -1,9 +1,10 @@
 let num1 = +prompt("First number");
 let num2 = +prompt("Second number");
 let op;
+
 do {
     op = prompt("Please input operator: +, -, *, /");
-} while (op !== "+" && op !== "-" && op !== "*" && op !== "/");
+} while (!(op === "+" || op === "-" || op === "*" || op === "/"));
 
 function calc(a, b, oper) {
     switch (oper) {
